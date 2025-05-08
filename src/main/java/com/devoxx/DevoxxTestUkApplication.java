e4c92084b2b873015630bb9bdbda655f02e330d7
Makan Sepehrifar <makan1869@gmail.com>
2025-05-08 16:43:03 +0200

    Initial project setup with Spring Boot

---
+++ src/main/java/com/devoxx/DevoxxTestUkApplication.java
@@ -0,0 +1,13 @@
+package com.devoxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoxxTestUkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoxxTestUkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devoxx/DevoxxTestUkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devoxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoxxTestUkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
